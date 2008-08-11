a9940af350db4a210ed6a81aa147712dac1926fd
komma8komma1 <komma8komma1@f5ecc899-f232-0410-8e71-85cf30c42a8c>
2008-08-11 19:19:56 +0000

    git-svn-id: https://svn.code.sf.net/p/mygeneration/code/trunk@201 f5ecc899-f232-0410-8e71-85cf30c42a8c

--- mygeneration/MyGenUtility/ZeusProcessManager.cs
+++ mygeneration/MyGenUtility/ZeusProcessManager.cs
@@ -5,16 +5,66 @@ using System.Text;
 using System.Threading;
 using System.Diagnostics;
 
+using Zeus;
+using MyGeneration;
+
 namespace MyGeneration
 {
+    public delegate void ZeusProcessCompleteDelegate(bool success);
+
     public static class ZeusProcessManager
     {
-        private static Queue<ZeusProcess> processes;
+        private static Queue<Process> processes;
 
+        public void ExecuteTemplate(string filename, ZeusProcessCompleteDelegate zpcd)
+        {
+            //
+        }
     }
-
+    public enum ZeusProcessType
+    {
+        ExecuteTemplate = 0
+    }
     public class ZeusProcess
     {
-        //Process
+        private ParameterizedThreadStart ts;
+        private Process process;
+
+        public ZeusProcess(ZeusProcessType type, params string[] args)
+        {
+            ProcessStartInfo si = new ProcessStartInfo();
+            if (type == ZeusProcessType.ExecuteTemplate)
+            {
+                if (args.Length > 0)
+                {
+                    si.FileName = FileTools.ApplicationPath + "\\ZeusCmd.exe";
+                    si.CreateNoWindow = true;
+                    si.UseShellExecute = false;
+                    si.RedirectStandardOutput = true;
+                    si.Arguments = "-t " + args[0];
+                }
+            }
+            process = new Process();
+            process.StartInfo = si;
+            ts = new ParameterizedThreadStart(Start);
+            ts.Invoke(process);
+        }
+
+        private void Start(object o)
+        {
+            if (o is Process)
+            {
+                Process p = o as Process;
+                p.Start();
+                while (!p.HasExited)
+                {
+                    string l = p.StandardOutput.ReadLine();
+                    // need to bubble up this line, and the status in a thread safe way.
+
+                    // if marked to "kill", break out and kill process!
+                }
+
+            }
+        }
     }
 }
