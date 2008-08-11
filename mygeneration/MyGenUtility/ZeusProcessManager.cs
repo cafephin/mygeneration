6316cb473a6d968c9aa0a05ceea14926fa089771
komma8komma1 <komma8komma1@f5ecc899-f232-0410-8e71-85cf30c42a8c>
2008-08-11 11:20:39 +0000

    git-svn-id: https://svn.code.sf.net/p/mygeneration/code/trunk@200 f5ecc899-f232-0410-8e71-85cf30c42a8c

---
+++ mygeneration/MyGenUtility/ZeusProcessManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace MyGeneration
+{
+    public static class ZeusProcessManager
+    {
+        private static Queue<ZeusProcess> processes;
+
+    }
+
+    public class ZeusProcess
+    {
+        //Process
+    }
+}
