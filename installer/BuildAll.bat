55864b46e34bf04d8cd35a63ef765e40669247c7
komma8komma1 <komma8komma1@f5ecc899-f232-0410-8e71-85cf30c42a8c>
2010-07-12 04:20:16 +0000

    added 64 bit 7 support for builds
    git-svn-id: https://svn.code.sf.net/p/mygeneration/code/trunk@303 f5ecc899-f232-0410-8e71-85cf30c42a8c

--- installer/BuildAll.bat
+++ installer/BuildAll.bat
@@ -3,14 +3,22 @@ del ".\installbuild_mygen.log"
 del ".\installbuild_mymeta.log"
 del ".\installbuild_doodads.log"
 
-if "%DEVENV%"=="" set DEVENV="%PROGRAMFILES%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"  /out ".\build.log" /rebuild release
+if "%PROGRAMFILES(X86)%"=="" goto :x86
+goto :x64
+:x86
+	if "%DEVENV%"=="" set DEVENV="%PROGRAMFILES%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"  /out ".\application_build.log" /rebuild release
+	if "%MAKENSIS%"=="" set MAKENSIS=%PROGRAMFILES%\NSIS
+	goto done
+:x64
+	if "%DEVENV%"=="" set DEVENV="%PROGRAMFILES(X86)%\Microsoft Visual Studio 10.0\Common7\IDE\devenv.exe"  /out ".\application_build.log" /rebuild release
+	if "%MAKENSIS%"=="" set MAKENSIS=%PROGRAMFILES(X86)%\NSIS
+:done
 
 %DEVENV% "..\plugins\MyMetaPlugins.sln"
 %DEVENV% "..\plugins\ZeusPlugins.sln"
 %DEVENV% "..\mygeneration\Zeus.sln"
 set DEVENV=
 
-if "%MAKENSIS%"=="" set MAKENSIS=%PROGRAMFILES%\NSIS\makensis.exe
 "%MAKENSIS%" ".\mygeneration.nsi" > ".\installbuild_mygen.log"
 "%MAKENSIS%"  ".\mymeta.nsi" > ".\installbuild_mymeta.log"
 "%MAKENSIS%"  ".\doodads.nsi" > ".\installbuild_doodads.log"
