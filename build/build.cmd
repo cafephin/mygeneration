4cd755384b66d3f850a550a90cc33b48fb8c2a2e
Khai Nguyen <khai.nguyen@outlook.com>
2017-10-20 12:15:57 -0700

    build.cmd now takes a configuration arg

--- build/build.cmd
+++ build/build.cmd
@@ -1,23 +1,29 @@
-if exist ".\build.log" del ".\build.log"
+SETLOCAL ENABLEEXTENSIONS
+@ECHO OFF
 
-set BUILD=msbuild /t:Rebuild /p:Configuration=Release /flp:logfile=build.log;Append
+IF EXIST ".\build.log" DEL ".\build.log"
+
+SET config=%1
+
+SET BUILD=msbuild /t:Rebuild /p:Configuration=%config% /flp:logfile=build.log;Append
 
 %BUILD% "..\src\plugins\MyMetaPlugins.sln"
 %BUILD% "..\src\plugins\ZeusPlugins.sln"
-
-COPY "..\src\plugins\Dnp.Utils\bin\Release\Dnp.Utils.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyGeneration.UI.Plugins.CodeSmith2MyGen\bin\Release\MyGeneration.UI.Plugins.CodeSmith2MyGen.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyGeneration.UI.Plugins.SqlTool\bin\release\MyGeneration.UI.Plugins.SqlTool.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyMetaEffiprozPlugin\bin\release\MyMeta.Plugins.EffiProz.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyMetaIngres2006Plugin\bin\release\MyMeta.Plugins.Ingres2006.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyMetaSqlCePlugin\bin\release\MyMeta.Plugins.SqlCe.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyMetaSybaseASAPlugin\bin\release\MyMeta.Plugins.SybaseASA.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyMetaSybaseASEPlugin\bin\release\MyMeta.Plugins.SybaseASE.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyMetaTextFilePlugin\bin\release\MyMeta.Plugins.DelimitedText.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\MyMetaXsd3bPlugin\bin\release\MyMeta.Plugins.Xsd3b.dll" "..\src\lib\plugins"
-COPY "..\src\plugins\SampleUIPlugin\bin\release\MyGeneration.UI.Plugins.Sample.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\Dnp.Utils\bin\%config%\Dnp.Utils.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyGeneration.UI.Plugins.CodeSmith2MyGen\bin\%config%\MyGeneration.UI.Plugins.CodeSmith2MyGen.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyGeneration.UI.Plugins.SqlTool\bin\%config%\MyGeneration.UI.Plugins.SqlTool.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyMetaEffiprozPlugin\bin\%config%\MyMeta.Plugins.EffiProz.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyMetaIngres2006Plugin\bin\%config%\MyMeta.Plugins.Ingres2006.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyMetaSqlCePlugin\bin\%config%\MyMeta.Plugins.SqlCe.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyMetaSybaseASAPlugin\bin\%config%\MyMeta.Plugins.SybaseASA.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyMetaSybaseASEPlugin\bin\%config%\MyMeta.Plugins.SybaseASE.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyMetaTextFilePlugin\bin\%config%\MyMeta.Plugins.DelimitedText.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\MyMetaXsd3bPlugin\bin\%config%\MyMeta.Plugins.Xsd3b.dll" "..\src\lib\plugins"
+COPY "..\src\plugins\SampleUIPlugin\bin\%config%\MyGeneration.UI.Plugins.Sample.dll" "..\src\lib\plugins"
 
 %BUILD% "..\src\mygeneration\ZeusCmd.sln"
 %BUILD% "..\src\mygeneration\MyGeneration.sln"
 
-set BUILD=
+SET BUILD=
+
+ECHO ON
