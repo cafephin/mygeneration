8e2bdce07ce850adc7e7e46e3ac06e169ba9c3ea
Khai Nguyen <khai.nguyen@outlook.com>
2017-10-27 16:41:08 -0700

    Add MyGeneration.Configuration.UnitTest

---
+++ src/mygeneration/MyGeneration.Configuration.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyGeneration.Configuration.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyGeneration.Configuration.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c89ee73c-76af-4873-af6e-5c81d06106c4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/mygeneration/MyGeneration.Configuration.UnitTest/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyGeneration.Configuration.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SetEntryAssembly();
+            var defaultSettings = new DefaultSettings
+                                  {
+                                      DbConnectionSettings =
+                                      {
+                                          ConnectionInfoCollection = new List<ConnectionInfo>
+                                                                     {
+                                                                         new ConnectionInfo()
+                                                                         {
+                                                                             Name = "test",
+                                                                             ConnectionString =
+                                                                                 "test conn str",
+                                                                             DbDriver = "test db driver"
+                                                                         }
+                                                                     },
+                                          UserDatabaseAliases = { new DatabaseAlias(){DatabaseName = "appq.dev.app.local", Alias = "AppQ"}}
+                                      },
+                                      RecentFiles = { "file1.csgen", "file2.zeus" }
+                                  };
+            var xmlSerializer = new System.Xml.Serialization.XmlSerializer(defaultSettings.GetType());
+            xmlSerializer.Serialize(Console.Out, defaultSettings);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Use as first line in ad hoc tests (needed by XNA specifically)
+        /// </summary>
+        public static void SetEntryAssembly()
+        {
+            SetEntryAssembly(Assembly.GetCallingAssembly());
+        }
+
+        /// <summary>
+        /// Allows setting the Entry Assembly when needed. 
+        /// Use AssemblyUtilities.SetEntryAssembly() as first line in XNA ad hoc tests
+        /// </summary>
+        /// <param name="assembly">Assembly to set as entry assembly</param>
+        public static void SetEntryAssembly(Assembly assembly)
+        {
+            var manager = new AppDomainManager();
+            FieldInfo entryAssemblyfield = manager.GetType().GetField("m_entryAssembly", BindingFlags.Instance | BindingFlags.NonPublic);
+            entryAssemblyfield.SetValue(manager, assembly);
+
+            AppDomain domain = AppDomain.CurrentDomain;
+            FieldInfo domainManagerField = domain.GetType().GetField("_domainManager", BindingFlags.Instance | BindingFlags.NonPublic);
+            domainManagerField.SetValue(domain, manager);
+        }
+    }
+}
